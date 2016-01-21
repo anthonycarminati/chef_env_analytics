0186fe50a909f50d5a10eba8b5ea30680a2cf50f
Anthony Carminati <anthony@carminati.io>
2016-01-21 11:42:15 -0600

    added app directory, pip install integration, and aws codedeploy agent

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -7,8 +7,31 @@
 ## ----------
 ## Starts codedeploy agent
 ## ----------
-
+service "codedeploy-agent" do
+  action [:enable, :start]
+end
 
 ## ----------
 ## Install/Update Python packages via python-pip
 ## ----------
+# node[:pip_python_packages].each_pair do |pkg, version|
+#     execute "install-#{pkg}" do
+#         command "pip install #{pkg}==#{version}"
+#         not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
+#     end
+# end
+
+# Versionless
+node[:pip_python_packages].each_pair do |pkg, version|
+  execute "install-#{pkg}" do
+    command "pip install #{pkg}"
+    # not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
+  end
+end
+
+# node[:pip_python_packages].each do |pkg|
+#     execute "install-#{pkg}" do
+#         command "pip install #{pkg}"
+# #        not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
+#     end
+# end
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -7,8 +7,24 @@
 ## ----------
 ## Install latest version of CodeDeploy
 ## ----------
+remote_file "#{Chef::Config[:file_cache_path]}/codedeploy-install.sh" do
+  source "https://s3.amazonaws.com/aws-codedeploy-us-east-1/latest/install"
+  mode "0744"
+  owner "root"
+  group "root"
+end
 
+execute "install_codedeploy_agent" do
+  command "#{Chef::Config[:file_cache_path]}/codedeploy-install.sh auto"
+  user "root"
+end
 
 ## ----------
 ## Initialize application directory
 ## ----------
+# TODO: make initialized app directory dynamic, feed params from attributes file
+directory '/opt/appdir/' do
+  owner 'root'
+  group 'root'
+  recursive true
+end
